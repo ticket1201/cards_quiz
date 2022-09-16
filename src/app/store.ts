327dd798850e495974e8e09d6c2f629b7030de72
Anton Metlitsky <ticket1201@gmail.com>
2022-09-17 00:29:59 +0300

    feat: add appReducer to rootReducer, thunk middleware, types for thunks

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,30 @@
-import {combineReducers, legacy_createStore} from 'redux';
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux';
 import {authReducer} from '../features/Login/auth_reducer';
 import {appReducer} from './app_reducer';
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     app: appReducer
-     /// some here
+     /// add your reducer here
 })
 
-export const store = legacy_createStore(rootReducer) // u can add middleware after coma symbol
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+
+
+export type RootStateType = ReturnType<typeof store.getState>
+
+export type RootActionsType = AnyAction // Replace it with ACs types
+
+// types for custom dispatch hook
+
+export type RootDispatchType = ThunkDispatch<RootStateType, unknown, RootActionsType>
+
+
+// types to dispatch thunks from thunks
+
+export type RootThunkType<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, RootActionsType>
+
 
 // @ts-ignore
 window.store = store
