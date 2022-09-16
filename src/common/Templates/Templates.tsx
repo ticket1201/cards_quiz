30492d5a166d135689cd9b2449cecf18755be379
Anton Metlitsky <ticket1201@gmail.com>
2022-09-17 00:36:45 +0300

    feat: del unnecessary inside component

--- src/common/Templates/Templates.tsx
+++ src/common/Templates/Templates.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import SuperCheckbox from '../SuperCheckbox/SuperCheckbox';
-import SuperInputText from '../SuperInputText/SuperInputText';
-import SuperButton from '../SuperButton/SuperButton';
 import '../../app/App.css'
 
 const Templates = () => {
     return (
         <div className={'template'}>
-            <div>
-                <h2>Checkbox</h2>
-                <SuperCheckbox children={'Example'}/>
-            </div>
-            <div>
-                <h2>Input</h2>
-                <SuperInputText/>
-                <h2>Input Error</h2>
-                <SuperInputText error={'Error text'}/>
-            </div>
-            <div>
-                <h2>Button</h2>
-                <SuperButton children={'Example'}/>
-                <SuperButton children={'Example Red'} red={true}/>
-            </div>
-
-
-
+           template
         </div>
     );
 };
