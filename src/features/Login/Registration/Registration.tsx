48c1c0a75ce576ac5570da8af96d087e33fc7d60
Barash777 <barash.vertihvost@gmail.com>
2022-09-17 01:47:57 +0300

    feat: add register form

--- src/features/Login/Registration/Registration.tsx
+++ src/features/Login/Registration/Registration.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import {useForm, SubmitHandler} from 'react-hook-form';
+// import FormControl from '@mui/material/FormControl';
+// import Input from '@mui/material/Input';
+
+
+type RegisterFormType = {
+    email: string
+    password: string
+}
 
 const Registration = () => {
+    const {register, handleSubmit} = useForm<RegisterFormType>();
+
+    const onSubmit: SubmitHandler<RegisterFormType> = (data) => {
+        console.log(data);
+        alert('email = ' + data.email + ', pass = ' + data.password)
+    }
+
     return (
         <div>
-            Registration
+            {/*<form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl>
+                    <Input {...register('email', {required: true})}  />
+                    <Input type="password" {...register('password', {required: true, minLength: 8})}  />
+                    <Input type="submit"/>
+                </FormControl>
+            </form>*/}
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <input {...register('email', {required: true})}
+                           style={{color: 'black'}}
+                    />
+                </div>
+                <div>
+                    <input type={'password'}
+                           {...register('password', {required: true, minLength: 8})}
+                           style={{color: 'black'}}
+                    />
+                </div>
+                <input type="submit" style={{color: 'black'}}/>
+            </form>
         </div>
     );
 };
