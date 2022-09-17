3c06ba9d82ff90e916461e7aa1db2c0330c6ab7f
Barash777 <barash.vertihvost@gmail.com>
2022-09-17 14:45:27 +0300

    feat: add ACs into app_reducer

--- src/app/app_reducer.ts
+++ src/app/app_reducer.ts
@@ -1,17 +1,58 @@
-
-export const appReducer = (state = initialState, action: any): any => {
-    switch (action.type) {
-        default :
-            return state
-    }
-}
-
-//types
-
 const initialState = {
     status: 'idle' as RequestStatusType,
     error: null as null | string,
     isInitialized: false as boolean
 }
 
+export type AppInitialStateType = typeof initialState
+
+export const appReducer = (state: AppInitialStateType = initialState, action: AppActionType): AppInitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-ERROR':
+        case 'APP/SET-STATUS':
+        case 'APP/SET-IS-INITIALIZED':
+            return {
+                ...state,
+                ...action.payload
+            }
+        default :
+            return state
+    }
+}
+
+// types
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppIsInitializedActionType = ReturnType<typeof setAppIsInitializedAC>
+export type AppActionType =
+    SetAppErrorActionType
+    | SetAppStatusActionType
+    | SetAppIsInitializedActionType
+
+
+// ACs
+export const setAppErrorAC = (error: null | string) => {
+    return {
+        type: 'APP/SET-ERROR',
+        payload: {
+            error
+        }
+    } as const
+}
+export const setAppStatusAC = (status: RequestStatusType) => {
+    return {
+        type: 'APP/SET-STATUS',
+        payload: {
+            status
+        }
+    } as const
+}
+export const setAppIsInitializedAC = (isInitialized: boolean) => {
+    return {
+        type: 'APP/SET-IS-INITIALIZED',
+        payload: {
+            isInitialized
+        }
+    } as const
+}
