d44fd1a6aab4ed2cc513e7dfc4ac2372aece87d8
Anton Metlitsky <ticket1201@gmail.com>
2022-09-17 13:31:16 +0300

    feat: add Profile component structure

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import {useAppSelector} from '../../common/hooks/hooks';
+import s from './Profile.module.css'
+import ava from '../../assets/images/avatar.jpg'
+import {EditableSpan} from '../../common/components/EditableSpan/EditableSpan';
+
 
 const Profile = () => {
+    const {name,email, /*avatar*/} = useAppSelector(state => state.auth)
     return (
-        <div>
-            Profile
-        </div>
+        <Paper className={'defaultPop'} elevation={2}>
+           <h2>Personal Information</h2>
+            <div className={s.ava} style={{backgroundImage:`url(${ava})`}}>
+                <div className={s.buttonWrapper}>
+                    <IconButton className={s.button}>+</IconButton>
+                </div>
+            </div>
+            <EditableSpan value={name} disabled={false} onChange={()=>{}}/>
+            <p>{email}</p>
+            <Button>Log Out</Button>
+        </Paper>
     );
 };
 
