d3fac85769cc7e6739a57115f1173c921255d522
Barash777 <barash.vertihvost@gmail.com>
2022-09-17 14:51:14 +0300

    feat: add error message into ErrorSnackbar

--- src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
+import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
+import {setAppErrorAC} from '../../../app/app_reducer';
 
 export const ErrorSnackbar = () => {
-    const handleClose = () => {}
-    const isError = false
+    const dispatch = useAppDispatch()
+    const error = useAppSelector(state => state.app.error)
+
+    const handleClose = () => {
+        // reset error after 6 seconds
+        dispatch(setAppErrorAC(null))
+    }
 
     return (
-        <Snackbar open={isError} autoHideDuration={6000} onClose={handleClose}>
-            <Alert variant="filled" severity="error">This is an error message!</Alert>
+        <Snackbar open={!!error} autoHideDuration={6000} onClose={handleClose}>
+            <Alert variant="filled" severity="error">{error}</Alert>
         </Snackbar>
     );
 };
