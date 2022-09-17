2165fc7a80b225c8f7ca2edd10fc93f144ce03c8
Barash777 <barash.vertihvost@gmail.com>
2022-09-17 15:10:57 +0300

    feat: add loading into header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,21 +4,30 @@ import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
+import LinearProgress from '@mui/material/LinearProgress';
+import {useAppSelector} from '../../hooks/hooks';
 
 const Header = () => {
+    const status = useAppSelector(state => state.app.status)
+
     return (
-        <AppBar position="absolute" className={s.bar} style={{backgroundColor: '#FCFCFC'}}>
-            <Toolbar className={s.toolbar}>
-                <Grid container
-                      direction="row"
-                      justifyContent="space-between"
-                      alignItems="center"
-                >
-                    <div className={s.logo}></div>
-                    <Button variant={'contained'}>Login</Button>
-                </Grid>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="relative" className={s.bar} style={{backgroundColor: '#FCFCFC'}}>
+                <Toolbar className={s.toolbar}>
+                    <Grid container
+                          direction="row"
+                          justifyContent="space-between"
+                          alignItems="center"
+                    >
+                        <div className={s.logo}></div>
+                        <Button variant={'contained'}>Login</Button>
+                    </Grid>
+                </Toolbar>
+                <div>
+                    {status === 'loading' && <LinearProgress/>}
+                </div>
+            </AppBar>
+        </>
     );
 };
 
