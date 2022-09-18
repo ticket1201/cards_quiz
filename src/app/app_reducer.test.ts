4cc233420e4af0d09cf70418b83c2d1b9b4c8c1d
Barash777 <barash.vertihvost@gmail.com>
2022-09-18 03:40:16 +0300

    feat: add app-reducer test

---
+++ src/app/app_reducer.test.ts
@@ -0,0 +1,42 @@
+import {
+    AppInitialStateType,
+    appReducer,
+    RequestStatusType,
+    setAppErrorAC,
+    setAppIsInitializedAC,
+    setAppStatusAC
+} from './app_reducer';
+
+let startState: AppInitialStateType, endState: AppInitialStateType;
+
+beforeEach(() => {
+    startState = {
+        status: 'idle',
+        error: null,
+        isInitialized: false
+    }
+})
+
+
+test('set new app status', () => {
+    const newStatus: RequestStatusType = 'loading'
+    endState = appReducer(startState, setAppStatusAC(newStatus))
+
+    expect(startState.status).toBe('idle')
+    expect(endState.status).toBe(newStatus)
+})
+
+test('set app error', () => {
+    const newError = 'error'
+    endState = appReducer(startState, setAppErrorAC(newError))
+
+    expect(startState.error).toBe(null)
+    expect(endState.error).toBe(newError)
+})
+
+test('set app isInitialized', () => {
+    endState = appReducer(startState, setAppIsInitializedAC(true))
+
+    expect(startState.isInitialized).toBe(false)
+    expect(endState.isInitialized).toBe(true)
+})
