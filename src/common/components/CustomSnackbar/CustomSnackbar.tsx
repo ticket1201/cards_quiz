36f7fa246253b86bae8f6543d4e029e07f05ddf7
Anton Metlitsky <ticket1201@gmail.com>
2022-09-20 01:48:11 +0300

    feat: some unnecessary changes

--- src/common/components/CustomSnackbar/CustomSnackbar.tsx
+++ src/common/components/CustomSnackbar/CustomSnackbar.tsx
@@ -9,18 +9,18 @@ const CustomSnackbar = () => {
     const dispatch = useAppDispatch()
     const error = useAppSelector(state => state.app.error)
     const success = useAppSelector(state => state.app.success)
+
+    const isMessage = error || success || null
     const severity: AlertColor = success ? 'success' : 'error'
-    const message = success ? success : error ? error : ''
 
     const handleClose = () => {
-        // reset error after 6 seconds
         error && dispatch(setAppErrorAC(null))
         success && dispatch(setAppSuccessAC(null))
     }
 
     return (
-        <Snackbar open={!!error} autoHideDuration={15000} onClose={handleClose}>
-            <Alert variant="filled" severity={severity}>{message}</Alert>
+        <Snackbar open={!!error || !!success} autoHideDuration={1000} onClose={handleClose}>
+            <Alert variant="filled" severity={severity}>{isMessage}</Alert>
         </Snackbar>
     );
 };
