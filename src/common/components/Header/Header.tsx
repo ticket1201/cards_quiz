24c19d89346893f876356a01a5d42e1271539806
Anton Metlitsky <ticket1201@gmail.com>
2022-09-20 01:49:34 +0300

    feat: del LinearProgress from Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,20 +4,17 @@ import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
-import LinearProgress from '@mui/material/LinearProgress';
+
 import {useAppSelector} from '../../hooks/hooks';
 import {NavLink, useNavigate} from 'react-router-dom';
 import ava from '../../../assets/images/avatar.jpg'
 
 const Header = () => {
-    const status = useAppSelector(state => state.app.status)
-
     const {name, avatar} = useAppSelector(state => state.auth)
     const navigate = useNavigate()
     const HandlerClick = () => {
         return navigate('/')
     }
-
 
     return (
         <AppBar position="absolute" className={s.bar} style={{backgroundColor: '#FCFCFC'}}>
@@ -31,14 +28,11 @@ const Header = () => {
                     {name
                         ? <div className={s.avaWrapper}>
                             <NavLink to={'/profile'}>{name}</NavLink>
-                            <NavLink className={s.ava} style={{backgroundImage:`url(${ava})`}} to={'/profile'}></NavLink>
+                            <NavLink className={s.ava} style={{backgroundImage:`url(${avatar || ava})`}} to={'/profile'}></NavLink>
                         </div>
                         : <Button variant={'contained'} onClick={() => HandlerClick()}>Login</Button>}
                 </Grid>
             </Toolbar>
-            <div>
-                {status === 'loading' && <LinearProgress/>}
-            </div>
         </AppBar>
     );
 };
