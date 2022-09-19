55d2eeb2a11bfe7619855ad13e0b2f7274760aa4
Anton Metlitsky <ticket1201@gmail.com>
2022-09-19 19:46:48 +0300

    feat: add avatar,name, button to Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,18 +4,34 @@ import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
+import {useAppSelector} from '../../hooks/hooks';
+import {NavLink, useNavigate} from 'react-router-dom';
+import ava from '../../../assets/images/avatar.jpg'
 
 const Header = () => {
+    const {name, avatar} = useAppSelector(state => state.auth)
+    const navigate = useNavigate()
+    const HandlerClick = () => {
+        return navigate('/')
+    }
+
+
     return (
         <AppBar position="absolute" className={s.bar} style={{backgroundColor: '#FCFCFC'}}>
-            <Toolbar className={s.toolbar}>
+            <Toolbar className={`${s.toolbar} base-wrapper`}>
                 <Grid container
                       direction="row"
                       justifyContent="space-between"
                       alignItems="center"
                 >
-                    <div className={s.logo}></div>
-                    <Button variant={'contained'}>Login</Button>
+                    <NavLink className={s.logo} to={'/'}></NavLink>
+                    {name
+                        ? <div className={s.avaWrapper}>
+                            <NavLink to={'/profile'}>{name}</NavLink>
+                            <NavLink className={s.ava} style={{backgroundImage:`url(${ava})`}} to={'/profile'}></NavLink>
+                        </div>
+                        : <Button variant={'contained'} onClick={() => HandlerClick()}>Login</Button>}
+
                 </Grid>
             </Toolbar>
         </AppBar>
