ac7fae0ce84dc170795589762cd7abb33ff38531
Anton Metlitsky <ticket1201@gmail.com>
2022-09-19 19:43:12 +0300

    feat: implement Profile component

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -2,26 +2,40 @@ import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
-import {useAppSelector} from '../../common/hooks/hooks';
+import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
 import s from './Profile.module.css'
 import ava from '../../assets/images/avatar.jpg'
 import {EditableSpan} from '../../common/components/EditableSpan/EditableSpan';
+import {LogoutTC} from '../Login/auth_reducer';
+import {Navigate} from 'react-router-dom';
 
 
 const Profile = () => {
-    const {name,email, /*avatar*/} = useAppSelector(state => state.auth)
+    const {_id, name,email, /*avatar*/} = useAppSelector(state => state.auth)
+    const dispatch = useAppDispatch()
+
+    const HandlerLogOut = () => {
+        dispatch(LogoutTC())
+    }
+
+    if(!_id){
+      return <Navigate to={'/'}/>
+    }
+
     return (
-        <Paper className={'defaultPop'} elevation={2}>
-           <h2>Personal Information</h2>
-            <div className={s.ava} style={{backgroundImage:`url(${ava})`}}>
-                <div className={s.buttonWrapper}>
-                    <IconButton className={s.button}>+</IconButton>
+        <div className={'base-wrapper'}>
+            <Paper className={'defaultPop'} elevation={2}>
+                <h2>Personal Information</h2>
+                <div className={s.ava} style={{backgroundImage:`url(${ava})`}}>
+                    <div className={s.buttonWrapper}>
+                        <IconButton className={s.button}>+</IconButton>
+                    </div>
                 </div>
-            </div>
-            <EditableSpan value={name} disabled={false} onChange={()=>{}}/>
-            <p>{email}</p>
-            <Button>Log Out</Button>
-        </Paper>
+                <EditableSpan value={name} disabled={false} onChange={()=>{}}/>
+                <p className={s.email}>{email}</p>
+                <Button variant={'outlined'} className={s.logOut} startIcon={'+'} onClick={()=>HandlerLogOut()}>Log Out</Button>
+            </Paper>
+        </div>
     );
 };
 
