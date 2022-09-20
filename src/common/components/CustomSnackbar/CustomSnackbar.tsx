cbcf7961d206cd6941f766687d607393ebc1fafd
Anton Metlitsky <ticket1201@gmail.com>
2022-09-20 15:46:28 +0300

    feat: implement UniversalSnackbar component

--- src/common/components/CustomSnackbar/CustomSnackbar.tsx
+++ src/common/components/CustomSnackbar/CustomSnackbar.tsx
@@ -5,25 +5,6 @@ import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
 import {setAppErrorAC, setAppSuccessAC} from '../../../app/app_reducer';
 import {AlertColor} from '@mui/material/Alert/Alert';
 
-export const SuccessSnackbar = () => {
-    const dispatch = useAppDispatch()
-    const success = useAppSelector(state => state.app.success)
-    const severity: AlertColor = success ? 'success' : 'error'
-    const message = success ? success : error ? error : ''
-
-    const handleClose = () => {
-        // reset error after 6 seconds
-        error && dispatch(setAppErrorAC(null))
-        success && dispatch(setAppSuccessAC(null))
-    }
-
-    return (
-        <Snackbar open={!!error} autoHideDuration={15000} onClose={handleClose}>
-            <Alert variant="filled" severity={severity}>{message}</Alert>
-        </Snackbar>
-    );
-};
-
 export const UniversalSnackbar = () => {
     const dispatch = useAppDispatch()
     const error = useAppSelector(state => state.app.error)
