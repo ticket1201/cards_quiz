a3317de3348dde48789214c7974ec6350c732406
Barash777 <barash.vertihvost@gmail.com>
2022-09-20 17:02:20 +0300

    feat: add PasswordInput into the form and move form as separate component

--- src/features/Login/Registration/Registration.tsx
+++ src/features/Login/Registration/Registration.tsx
@@ -11,6 +11,7 @@ import Button from '@mui/material/Button';
 import s from '../Login/Login.module.css';
 import FormLabel from '@mui/material/FormLabel';
 import FormGroup from '@mui/material/FormGroup';
+import {PasswordInput} from '../../../common/components/PasswordInput/PasswordInput';
 // import {setAppErrorAC, setAppStatusAC} from '../../../app/app_reducer';
 // import PasswordInput from '../../../common/components/PasswordInput/PasswordInput';
 
@@ -22,7 +23,35 @@ export type RegisterFormType = {
 }
 
 const Registration = () => {
-    const {register, handleSubmit, watch, formState: {errors}} = useForm<RegisterFormType>();
+    return (
+        <div className={'base-wrapper'}>
+            <Paper className={'defaultPop'} elevation={2}>
+                <FormControl className={s.FormControl}>
+                    <FormLabel>
+                        <h2 className={s.title}>Sign up</h2>
+                    </FormLabel>
+
+                    <RegisterForm/>
+
+                </FormControl>
+                <p className={s.text}>Already have an account ?</p>
+                <NavLink to={'/login'} className={s.signUpLink}>Sing in</NavLink>
+            </Paper>
+        </div>
+    );
+};
+
+export default Registration;
+
+const RegisterForm = () => {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm<RegisterFormType>({
+        defaultValues: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        mode: 'onTouched'
+    });
     let password = watch('password', '')
 
     const dispatch = useAppDispatch()
@@ -44,68 +73,42 @@ const Registration = () => {
     }
 
     return (
-        <div className={'base-wrapper'}>
-            <Paper className={'defaultPop'} elevation={2}>
-                <FormControl className={s.FormControl}>
-                    <FormLabel>
-                        <h2 className={s.title}>Sign up</h2>
-                    </FormLabel>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.wrapper}
+              onKeyDown={(e) => onEnterPress(e.key)}>
+            <FormGroup>
+                <TextField
+                    label="Email"
+                    variant="standard"
+                    margin="normal"
+                    {...register('email', {
+                        required: 'Email is required',
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                            message: 'Write correct email'
+                        }
+                    })}
+                />
+                {errors.email && <div style={{color: 'red'}}>{errors.email.message}</div>}
 
-                    <form onSubmit={handleSubmit(onSubmit)} className={s.wrapper}
-                          onKeyDown={(e) => onEnterPress(e.key)}>
-                        <FormGroup>
-                            <TextField
-                                label="Email"
-                                variant="standard"
-                                margin="normal"
-                                {...register('email', {
-                                    required: 'Email is required',
-                                    pattern: {
-                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                                        message: 'Write correct email'
-                                    }
-                                })}
-                            />
-                            {errors.email && <div style={{color: 'red'}}>{errors.email.message}</div>}
-                            <TextField
-                                label="Password"
-                                variant="standard"
-                                margin="normal"
-                                type={'password'}
-                                {...register('password', {
-                                    required: 'Password is required',
-                                    minLength: {
-                                        value: 8,
-                                        message: 'Min length is 8'
-                                    }
-                                })}
-                            />
-                            {errors.password && <div style={{color: 'red'}}>{errors.password.message}</div>}
-                            <TextField
-                                label="Confirm password"
-                                variant="standard"
-                                margin="normal"
-                                type={'password'}
-                                {...register('confirmPassword', {
-                                    validate: value =>
-                                        value === password || 'The passwords do not match'
-                                })}
-                            />
-                            {errors.confirmPassword &&
-                                <div style={{color: 'red'}}>{errors.confirmPassword.message}</div>}
+                <PasswordInput register={register('password', {
+                    required: 'Password is required', minLength: {
+                        value: 8, message: 'Password must be more than 8 characters'
+                    }
+                })} label={'Password'}/>
+                {errors.password && <div style={{color: 'red'}}>{errors.password.message}</div>}
 
-                            <Button type={'submit'} variant={'contained'} color={'primary'} style={{marginTop: '40px'}}
-                                    fullWidth>
-                                Sign up
-                            </Button>
-                        </FormGroup>
-                    </form>
-                </FormControl>
-                <p className={s.text}>Already have an account ?</p>
-                <NavLink to={'/login'} className={s.signUpLink}>Sing in</NavLink>
-            </Paper>
-        </div>
-    );
-};
+                <PasswordInput register={register('confirmPassword', {
+                    validate: value =>
+                        value === password || 'The passwords do not match'
+                })} label={'Confirm password'}/>
+                {errors.confirmPassword &&
+                    <div style={{color: 'red'}}>{errors.confirmPassword.message}</div>}
 
-export default Registration;
+                <Button type={'submit'} variant={'contained'} color={'primary'} style={{marginTop: '40px'}}
+                        fullWidth>
+                    Sign up
+                </Button>
+            </FormGroup>
+        </form>
+    )
+}
