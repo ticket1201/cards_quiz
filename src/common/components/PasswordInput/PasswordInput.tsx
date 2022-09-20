1307e5a60f903f684dff24f2abc771feeb293380
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-20 14:45:29 +0300

    feat: create PasswordInput component

--- src/common/components/PasswordInput/PasswordInput.tsx
+++ src/common/components/PasswordInput/PasswordInput.tsx
@@ -1,63 +1,53 @@
-import React, {useState} from 'react';
-import IconButton from '@mui/material/IconButton';
-// import OutlinedInput from '@mui/material/OutlinedInput';
-// import StandardInput from '@mui/material/StandardInput';
+import React, {FC, useState} from 'react';
+import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
-import Visibility from '@mui/icons-material/Visibility';
+import IconButton from '@mui/material/IconButton';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-// import {OutlinedInputProps} from '@mui/material/OutlinedInput/OutlinedInput';
-// import {TextFieldProps} from '@mui/material/TextField/TextField';
-// import TextField from '@mui/material/TextField';
-import Input from '@mui/material/Input';
-import {FormControl, InputLabel} from '@mui/material';
-import {InputProps} from '@mui/material/Input/Input';
+import Visibility from '@mui/icons-material/Visibility';
 
-type PasswordInputType = InputProps & {
-    // showPassword: boolean
-    //password: string
+type PasswordInputPropsType = {
+    register: any
+    label: string
 }
+export const PasswordInput: FC<PasswordInputPropsType> = ({register, label}) => {
 
-const PasswordInput = (/*{password}: PasswordInputType*/props: PasswordInputType) => {
-    // const [value, setValue] = useState<string>();
-    const [showPassword, setShowPassword] = useState<boolean>(false);
-
-    /*const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value);
-    };*/
-
+    const [values, setValues] = useState({
+        password: '',
+        showPassword: false
+    })
     const handleClickShowPassword = () => {
-        setShowPassword(!showPassword);
-    };
-
+        setValues({
+            ...values, showPassword: !values.showPassword
+        })
+    }
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
     };
-
-    return (
-        <FormControl variant="standard">
-            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-            <Input
-                {...props}
-                id="standard-adornment-password"
-                type={showPassword ? 'text' : 'password'}
-                // value={value}
-                // onChange={handleChange}
-
-                endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                        >
-                            {showPassword ? <VisibilityOff/> : <Visibility/>}
-                        </IconButton>
-                    </InputAdornment>
-                }
-
-            />
-        </FormControl>
-    );
-};
-
-export default PasswordInput;
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({
+            ...values, password: event.currentTarget.value
+        })
+    };
+    return <TextField type={values.showPassword ? 'text' : 'password'}
+                      label={label}
+                      margin="normal"
+                      variant="standard"
+                      InputProps={{
+                          endAdornment: <InputAdornment position={'end'}>
+                              <IconButton
+                                  aria-label="toggle password visibility"
+                                  onClick={handleClickShowPassword}
+                                  onMouseDown={handleMouseDownPassword}
+                              >
+                                  {values.showPassword ? <VisibilityOff/> : <Visibility/>}
+                              </IconButton>
+                          </InputAdornment>
+                      }}
+                      {...register('password', {
+                          value: values.password, onChange: handleChange,
+                          required: 'Password is required', minLength: {
+                              value: 8, message: 'Password must be more than 8 characters'
+                          }
+                      })}
+    />
+}
