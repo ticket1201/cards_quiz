ab8da86db963b53d524189b8fa04fae5778d6c91
Anton Metlitsky <ticket1201@gmail.com>
2022-09-21 17:32:00 +0300

    feat: add return value to ForgotPassTC

--- src/features/Login/auth_reducer.ts
+++ src/features/Login/auth_reducer.ts
@@ -104,7 +104,7 @@ export const logoutTC = (): RootThunkType => async (dispatch) => {
     }
 }
 
-export const ForgotPassTC = ({email}: { email: string }): RootThunkType => async (dispatch) => {
+export const ForgotPassTC = ({email}: { email: string }): RootThunkType<Promise<boolean>> => async (dispatch) => {
     // replace url in href to your gh-page address
     const message = {'message': '<div style="padding: 15px; font-weight: bold">\n Trouble signing in? <br>\n Resetting your password is easy. <br>\n Just click on link below and follow the instructions. We’ll have you up and running in no time.  <br>\n \n <a href="https://ticket1201.github.io/cards_quiz/#/set-new-password/$token$">Link</a><br>\n If you did not make this request then please ignore this email.\n</div>'}
 
@@ -112,8 +112,10 @@ export const ForgotPassTC = ({email}: { email: string }): RootThunkType => async
 
     try {
         await authAPI.forgotPassword({email, ...message})
+        return true
     } catch (e: any) {
         errorUtils(e, dispatch)
+        return false
     } finally {
         dispatch(setAppStatusAC('idle'))
     }
