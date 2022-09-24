0d6d369e6ffd953e69385ea0d3aaf5fc6287e997
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-24 15:42:49 +0300

    feat: create Pages component

---
+++ src/common/components/Pages/Pages.tsx
@@ -0,0 +1,26 @@
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Login} from '../../../features/Login/Login/Login';
+import React from 'react';
+import Profile from '../../../features/Profile/Profile';
+import Registration from '../../../features/Login/Registration/Registration';
+import ResetPassword from '../../../features/Login/ResetPassword/ResetPassword';
+import Success from '../../../features/Login/Success/Success';
+import NewPassword from '../../../features/Login/ResetPassword/NewPassword/NewPassword';
+import Error404 from '../../../features/Error404/Error404';
+import {Path} from '../../enums/path';
+import {PrivateRoutes} from '../PrivateRoutes/PrivateRoutes';
+
+export const Pages = () => {
+    return <Routes>
+        <Route element={<PrivateRoutes/>}>
+            <Route path={Path.Profile} element={<Profile/>}/>
+            <Route path={`${Path.ResetSuccess}/:email`} element={<Success/>}/>
+        </Route>
+        <Route path={Path.Login} element={<Login/>}/>
+        <Route path={Path.Registration} element={<Registration/>}/>
+        <Route path={Path.ResetPassword} element={<ResetPassword/>}/>
+        <Route path={`${Path.NewPassword}/:token`} element={<NewPassword/>}/>
+        <Route path={Path.Error404} element={<Error404/>}/>
+        <Route path={Path.Other} element={<Navigate to={Path.Error404}/>}/>
+    </Routes>
+};
