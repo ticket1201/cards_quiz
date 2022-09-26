ab79067cad05a0a3a6196c6ccbba0841f96583e0
Anton Metlitsky <ticket1201@gmail.com>
2022-09-26 22:15:55 +0300

    feat: implement cards_reducer

---
+++ src/features/PackPage/cards_reducer.ts
@@ -0,0 +1,66 @@
+import {
+    CardDataType, CardRequestType,
+    cardsAPI, GetCardParamsType,
+    getCardsResponseType, UpdateCardRequestType,
+} from '../../api/api';
+import {RootThunkType} from '../../app/store';
+import {setAppStatusAC} from '../../app/app_reducer';
+import {errorUtils} from '../../common/utils/error-utils';
+import {universalPacksCardsTC} from '../../common/utils/universalPacksCardsTC';
+
+
+const initialState = {
+    cards: [] as Array<CardDataType>,
+    packName: '',
+    cardsTotalCount: 0,
+    maxGrade: 0,
+    minGrade: 0,
+    page: 0,
+    pageCount: 0,
+    packUserId: ''
+}
+
+
+export const cardsReducer = (state: CardsInitialStateType = initialState, action: CardsActionType): CardsInitialStateType => {
+    switch (action.type) {
+        case 'CARDS/SET_CARDS':
+            return {...state, ...action.payload}
+        default :
+            return state
+    }
+}
+
+//types
+export type CardsInitialStateType = typeof initialState
+export type CardsActionType =
+    | ReturnType<typeof setCardsAC>
+
+// ACs
+export const setCardsAC = (payload: getCardsResponseType) => {
+    return {
+        type: 'CARDS/SET_CARDS',
+        payload
+    } as const
+}
+
+//TCs
+
+export const getCardsTC = (data: GetCardParamsType): RootThunkType => async (dispatch) => {
+    setAppStatusAC('loading')
+    try {
+        const res = await cardsAPI.getCards(data)
+        dispatch(setCardsAC(res.data))
+    } catch (e: any) {
+        errorUtils(e, dispatch)
+    } finally {
+        setAppStatusAC('idle')
+    }
+}
+
+export const createCardTC = (data: CardRequestType) => universalPacksCardsTC('cards', cardsAPI.createCard, data)
+
+export const updateCardTC = (data: UpdateCardRequestType) => universalPacksCardsTC('cards', cardsAPI.updateCard, data)
+
+export const deleteCardTC = (id: string) => universalPacksCardsTC('cards', cardsAPI.deleteCard, id)
+
+
