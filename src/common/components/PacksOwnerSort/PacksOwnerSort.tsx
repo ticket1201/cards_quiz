fb347ca4e545795f0cce65c651520b3c732a625f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-27 22:31:07 +0300

    refactor: update import in PacksOwnerSort component

--- src/common/components/PacksOwnerSort/PacksOwnerSort.tsx
+++ src/common/components/PacksOwnerSort/PacksOwnerSort.tsx
@@ -2,16 +2,17 @@ import React, {useEffect} from 'react';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
-import {getPacksTC, sortPacksByOwnerAC} from '../../../features/PacksList/pack_reducer';
+import {getPacksTC} from '../../../features/PacksList/pack_reducer';
+import {sortPacksByOwnerAC} from '../../../features/SearchBar/search-reducer';
 
 export const PacksOwnerSort = () => {
     const dispatch = useAppDispatch()
     const userId = useAppSelector(state => state.auth._id)
-    const ownerId = useAppSelector(state => state.packs.user_id)
+    const ownerId = useAppSelector(state => state.search.user_id)
 
     useEffect(() => {
-        dispatch(getPacksTC())
-    }, [ownerId])
+        dispatch(getPacksTC({user_id: ownerId}))
+    }, [ownerId, dispatch])
 
     const getMyPacksHandler = () => dispatch(sortPacksByOwnerAC(userId))
     const getAllPacksHandler = () => dispatch(sortPacksByOwnerAC())
