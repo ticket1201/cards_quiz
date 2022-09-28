be462ed5ee59cb624f916ee8368f59557f56af09
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-28 15:09:16 +0300

    refactor: remove useEffect and dispatch

--- src/common/components/PacksOwnerSort/PacksOwnerSort.tsx
+++ src/common/components/PacksOwnerSort/PacksOwnerSort.tsx
@@ -1,27 +1,26 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
-import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
-import {getPacksTC} from '../../../features/PacksList/pack_reducer';
-import {sortPacksByOwnerAC} from '../../../features/SearchBar/search-reducer';
+import {useAppSelector} from '../../hooks/hooks';
 
-export const PacksOwnerSort = () => {
-    const dispatch = useAppDispatch()
+type PacksOwnerSortType = {
+    owner: string | null
+    packsOwnerHandler: (ownerId: string) => void
+}
+
+export const PacksOwnerSort: React.FC<PacksOwnerSortType> = ({owner, packsOwnerHandler}) => {
+
     const userId = useAppSelector(state => state.auth._id)
-    const ownerId = useAppSelector(state => state.search.user_id)
 
-    useEffect(() => {
-        dispatch(getPacksTC({user_id: ownerId}))
-    }, [ownerId, dispatch])
 
-    const getMyPacksHandler = () => dispatch(sortPacksByOwnerAC(userId))
-    const getAllPacksHandler = () => dispatch(sortPacksByOwnerAC())
+    const getMyPacksHandler = () => userId && packsOwnerHandler(userId)
+    const getAllPacksHandler = () => packsOwnerHandler('')
 
     return (
         <div>
             <ButtonGroup>
-                <Button onClick={getMyPacksHandler} variant={ownerId ? 'contained' : 'outlined'}>My</Button>
-                <Button onClick={getAllPacksHandler} variant={ownerId ? 'outlined' : 'contained'}>All</Button>
+                <Button onClick={getMyPacksHandler} variant={owner ? 'contained' : 'outlined'}>My</Button>
+                <Button onClick={getAllPacksHandler} variant={owner ? 'outlined' : 'contained'}>All</Button>
             </ButtonGroup>
         </div>
     );
