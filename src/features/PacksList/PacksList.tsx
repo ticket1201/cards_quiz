a24c27dedfc3241ede28e16c907e7f7a3c040fbd
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-28 15:11:55 +0300

    feat: add filter components

--- src/features/PacksList/PacksList.tsx
+++ src/features/PacksList/PacksList.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import UniversalTable from '../../common/components/Table/UniversalTable';
 import {GridColDef} from '@mui/x-data-grid';
 import SchoolIcon from '@mui/icons-material/School';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
+import {useSearchParams} from 'react-router-dom';
+import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
+import {getPacksTC} from './pack_reducer';
+import {Search} from '../../common/components/Search/Search';
+import {RangeSlider} from '../../common/components/RangeSlider/RangeSlider';
+import {PacksOwnerSort} from '../../common/components/PacksOwnerSort/PacksOwnerSort';
+import {ClearFilters} from '../../common/components/ClearFilters/ClearFilters';
+import {Paginator} from '../../common/components/Paginator/Paginator';
 
 const columns: GridColDef[] = [
     {field: 'name', headerName: 'Name', flex: 1},
@@ -43,7 +51,7 @@ const columns: GridColDef[] = [
     },*/
 ];
 
-const rows = [
+/*const rows = [
     {id: '1', name: 'Anton\'s pack', cards: '4', lastUpdated: '18.02.2019', createdBy: 'Anton', actions: true},
     {id: '2', name: 'Artem\'s pack', cards: '7', lastUpdated: '09.04.2020', createdBy: 'Artem', actions: false},
     {id: '3', name: 'Siarhei\'s pack', cards: '1', lastUpdated: '21.01.2017', createdBy: 'Siarhei', actions: false},
@@ -60,13 +68,110 @@ const rows = [
     {id: '14', name: 'Artem\'s pack', cards: '7', lastUpdated: '09.04.2020', createdBy: 'Artem', actions: false},
     {id: '15', name: 'Siarhei\'s pack', cards: '1', lastUpdated: '21.01.2017', createdBy: 'Siarhei', actions: true},
 
-];
+];*/
 
 const PacksList = () => {
+    const dispatch = useAppDispatch()
+    const {
+        cardPacks,
+        minCardsCount,
+        maxCardsCount,
+        page,
+        pageCount,
+        cardPacksTotalCount
+    } = useAppSelector(state => state.packs)
+    let rows = cardPacks.map(el => ({
+        ...el, id: el._id
+    }))
+
+    const [searchParam, setSearchParam] = useSearchParams({})
+    let searchValue = searchParam.get('packName')
+    let min = searchParam.get('min')
+    let max = searchParam.get('max')
+    let user_id = searchParam.get('user_id')
+
+    // let somePage = searchParam.get('user_id')
+
+    const searchHandler = (value: string) => {
+        let temp = {}
+        // @ts-ignore
+        for (const [key, value] of searchParam.entries()) {
+            temp = {...temp, [key]: value}
+        }
+        if (!value) {
+            // @ts-ignore
+            delete temp.packName
+            setSearchParam(temp)
+            return
+        }
+        setSearchParam({...temp, packName: value})
+    }
+
+    const rangeHandler = (min: number, max: number) => {
+        let temp = {}
+        // @ts-ignore
+        for (const [key, value] of searchParam.entries()) {
+            temp = {...temp, [key]: value}
+        }
+        if (min === 0 && max === maxCardsCount) {
+            // @ts-ignore
+            delete temp.min
+            // @ts-ignore
+            delete temp.max
+            setSearchParam(temp)
+            return
+        }
+        setSearchParam({...temp, min: min.toString(), max: max.toString()})
+    }
+
+    const packsOwnerHandler = (user_id: string) => {
+        let temp = {}
+        // @ts-ignore
+        for (const [key, value] of searchParam.entries()) {
+            temp = {...temp, [key]: value}
+        }
+
+        if (!user_id) {
+            // @ts-ignore
+            delete temp.user_id
+            setSearchParam(temp)
+            return
+        }
+        setSearchParam({...temp, user_id})
+    }
+
+    const clearFiltersHandler = () => {
+        setSearchParam({})
+    }
+
+    const paginationHandler = () => {
+    }
+
+
+    let params = {}
+    searchParam.forEach((value: string, key: string) => {
+        params = {...params, [key]: value}
+    })
+
+
+    useEffect(() => {
+        let id = setTimeout(() => {
+            dispatch(getPacksTC(params))
+        }, 1000)
+        return () => clearTimeout(id)
+    }, [min, max, searchValue, user_id])
+
     return (
         <>
             Packs list
+            <Search isFullWidth={true} searchHandler={searchHandler} searchValue={searchValue}/>
+            <RangeSlider minValue={minCardsCount} maxValue={maxCardsCount} currentMin={min} currentMax={max}
+                         rangeSliderHandler={rangeHandler}/>
+            <PacksOwnerSort owner={user_id} packsOwnerHandler={packsOwnerHandler}/>
+            <ClearFilters clearHandler={clearFiltersHandler}/>
             <UniversalTable columns={columns} rows={rows} pageSize={10}/>
+            <Paginator changePageHandler={paginationHandler} currentPage={page} itemsOnPage={pageCount}
+                       itemsTotalCount={cardPacksTotalCount}/>
         </>
     );
 };
