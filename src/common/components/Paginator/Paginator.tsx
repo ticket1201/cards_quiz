8efb508f563da12ea06497f23b32462a3b58a5f7
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-28 21:43:09 +0300

    feat: create Paginator component

---
+++ src/common/components/Paginator/Paginator.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Pagination from '@mui/material/Pagination';
+import MenuItem from '@mui/material/MenuItem';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import s from './Paginator.module.css'
+
+type PaginatorPropsType = {
+    currentPage: number
+    itemsOnPage: number
+    itemsTotalCount: number
+    selectedPagesCount: string | null
+    changePageHandler: (page: number) => void
+    changePagesCountHandler: (pagesCount: string) => void
+}
+
+export const Paginator: React.FC<PaginatorPropsType> = ({
+                                                            currentPage,
+                                                            itemsOnPage,
+                                                            itemsTotalCount,
+                                                            selectedPagesCount,
+                                                            changePageHandler,
+                                                            changePagesCountHandler
+                                                        }) => {
+    const pagesCount = Math.ceil(itemsTotalCount / itemsOnPage)
+
+    const changePage = (event: React.ChangeEvent<unknown>, value: number) => {
+        changePageHandler(value)
+    }
+
+    const changeSelect = (event: SelectChangeEvent) => {
+        changePagesCountHandler(event.target.value)
+    }
+    return (
+        <div className={s.container}>
+            <Pagination count={pagesCount}
+                        page={currentPage}
+                        onChange={changePage}
+                        shape="rounded"
+                        color="primary"
+                        showFirstButton
+                        showLastButton/>
+            <div>
+                <span className={s.text}>Show</span>
+                <Select
+                    autoWidth
+                    size="small"
+                    value={selectedPagesCount ? selectedPagesCount : '4'}
+                    onChange={changeSelect}>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
+                    <MenuItem value={20}>20</MenuItem>
+                    <MenuItem value={50}>50</MenuItem>
+                    <MenuItem value={100}>100</MenuItem>
+                </Select>
+                <span className={s.text}>Cards per Page</span>
+            </div>
+        </div>
+    );
+};
