bf5752f572cb5ad82bb4ed17476fe695e1e711bc
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-28 15:12:50 +0300

    refactor: remove dispatch and reset prop

--- src/common/components/RangeSlider/RangeSlider.tsx
+++ src/common/components/RangeSlider/RangeSlider.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
+import {FC, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
-import {FC, useEffect} from 'react';
-import {getPacksTC} from '../../../features/PacksList/pack_reducer';
-import {AnyAction} from 'redux';
 import Button from '@mui/material/Button';
 import s from './RangeSlider.module.css'
 
@@ -15,50 +12,51 @@ function valuetext(value: number) {
 type RangeSliderType = {
     minValue: number
     maxValue: number
-    isReset: boolean
-    rangeAC: (min: number, max: number) => AnyAction
+    currentMin: string | null
+    currentMax: string | null
+    rangeSliderHandler: (min: number, max: number) => void
 }
 
-export const RangeSlider: FC<RangeSliderType> = ({minValue, maxValue, isReset, rangeAC}) => {
+export const RangeSlider: FC<RangeSliderType> = ({
+                                                     minValue,
+                                                     maxValue,
+                                                     currentMin,
+                                                     currentMax,
+                                                     rangeSliderHandler
+                                                 }) => {
 
-    const {min, max} = useAppSelector(state => state.search)
-    const dispatch = useAppDispatch()
     const [value, setValue] = React.useState<number[]>([]);
 
     useEffect(() => {
+        if (currentMin && currentMax) {
+            setValue([+currentMin, +currentMax])
+            return
+        }
         setValue([minValue, maxValue])
-    }, [minValue, maxValue])
-
-    useEffect(() => {
-        dispatch(getPacksTC({min, max}))
-    }, [min, max, dispatch])
+    }, [minValue, maxValue, currentMin, currentMax])
 
     const handleChange = (event: React.SyntheticEvent | Event, newValue: number | number[]) => {
         setValue(newValue as number[]);
     };
 
     const handleChangeCommitted = () => {
-        dispatch(rangeAC(value[0], value[1]))
+        rangeSliderHandler(value[0], value[1])
     }
 
     const resetMinHandler = () => {
         if (value[0] !== minValue) {
             setValue([minValue, value[1]])
-            dispatch(rangeAC(minValue, value[1]))
+            rangeSliderHandler(minValue, value[1])
         }
     }
 
     const resetMaxHandler = () => {
         if (value[1] !== maxValue) {
             setValue([value[0], maxValue])
-            dispatch(rangeAC(value[0], maxValue))
+            rangeSliderHandler(value[0], maxValue)
         }
     }
 
-    if (isReset) {
-        resetMinHandler()
-        resetMaxHandler()
-    }
     return (
         <div className={s.container}>
             <Button variant={'outlined'} onClick={resetMinHandler}>{value[0]}</Button>
