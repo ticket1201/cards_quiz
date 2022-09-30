ca7e5ad793de75cd1209afef6a062a9e95c770a6
Anton Metlitsky <ticket1201@gmail.com>
2022-09-30 12:58:47 +0300

    feat: implement serializeFormQuery function

---
+++ src/common/utils/serializeFormQuery.ts
@@ -0,0 +1,12 @@
+export const serializeFormQuery = (searchParam:URLSearchParams, authId?: string) => {
+    let temp = {}
+    searchParam.forEach((value: string, key: string) => {
+        if (authId && key === 'user_id') {
+            temp = {...temp, [key]: 'my'}
+            return
+        }
+        temp = {...temp, [key]: value}
+    })
+
+    return temp
+}
