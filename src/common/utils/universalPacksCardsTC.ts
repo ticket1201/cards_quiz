b5950e090f72f2e761d8bba5767fd81dfc4a01d3
Anton Metlitsky <ticket1201@gmail.com>
2022-09-30 03:59:40 +0300

    refactor: rework actionCreators, add dispatch to Status

--- src/common/utils/universalPacksCardsTC.ts
+++ src/common/utils/universalPacksCardsTC.ts
@@ -12,20 +12,18 @@ export const universalPacksCardsTC = <T>(reducerName: ReducerNameType, method: (
 
     switch (reducerName){
         case 'packs':
-            ActionCreator = () => setPacksIsChangedAC({isChanged: true})
+            ActionCreator = () => setPacksIsChangedAC()
             break
         case 'cards':
-            ActionCreator = () => setCardsIsChangedAC({isChanged: true})
+            ActionCreator = () => setCardsIsChangedAC()
             break
     }
 
-    setAppStatusAC('loading')
+    dispatch(setAppStatusAC('loading'))
     try {
         await method(model)
         dispatch(ActionCreator())
     } catch (e: any) {
         errorUtils(e, dispatch)
-    } finally {
-        setAppStatusAC('idle')
     }
 }
