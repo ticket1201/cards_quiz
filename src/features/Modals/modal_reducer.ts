3422914aba74cd0b76c64de557504dfe0ee3ccad
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-04 23:02:52 +0300

    feat: create modal reducer

---
+++ src/features/Modals/modal_reducer.ts
@@ -0,0 +1,52 @@
+const initialState = {
+    _id: '',
+    name: '',
+    openAddPackModal: false,
+    openEditPackModal: false,
+    openDelPackModal: false,
+    openAddCardModal: false,
+    openEditCardModal: false,
+    openDelCardModal: false
+}
+
+export type ModalInitialStateType = typeof initialState
+
+export const modalReducer = (state: ModalInitialStateType = initialState, action: ModalActionType): ModalInitialStateType => {
+    switch (action.type) {
+        case 'modal/OPEN-MODAL':
+            return {...state, ...action.payload}
+        case 'modal/CLOSE-MODAL':
+            return {
+                ...state,
+                _id: '',
+                openAddPackModal: false,
+                openEditPackModal: false,
+                openDelPackModal: false,
+                openAddCardModal: false,
+                openEditCardModal: false,
+                openDelCardModal: false
+            }
+        default:
+            return state
+    }
+}
+
+//types
+type ModalStateDataType = Partial<ModalInitialStateType>
+type ModalActionType =
+    ReturnType<typeof openModalAC>
+    | ReturnType<typeof closeModalAC>
+
+//ACs
+export const openModalAC = (payload: ModalStateDataType) => {
+    return {
+        type: 'modal/OPEN-MODAL',
+        payload
+    } as const
+}
+
+export const closeModalAC = () => {
+    return {
+        type: 'modal/CLOSE-MODAL'
+    } as const
+}
