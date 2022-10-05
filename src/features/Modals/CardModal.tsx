5f0646a8765bf510f7f8ec9c4396efd2f6c1c76c
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-05 21:50:58 +0300

    refactor: pass props to CardModal component, replace two inputs local states with one

--- src/features/Modals/CardModal.tsx
+++ src/features/Modals/CardModal.tsx
@@ -4,16 +4,22 @@ import {SubmitHandler, useForm} from 'react-hook-form';
 import FormGroup from '@mui/material/FormGroup';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import {InputLabel, Select, Stack} from '@mui/material';
-import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
-import {closeModalAC} from './modal_reducer';
+import InputLabel from '@mui/material/InputLabel';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
+import {useAppDispatch} from '../../common/hooks/hooks';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
-import {SelectChangeEvent} from '@mui/material/Select';
 import {createCardTC, updateCardTC} from '../PackPage/cards_reducer';
 
 type CardModalType = {
+    _id: string
+    cardsPack_id: string
+    question: string
+    answer: string
     title: string
+    isOpen: boolean
+    onClose: () => void
 }
 
 type CardModalFormData = {
@@ -21,19 +27,10 @@ type CardModalFormData = {
     answer: string
 }
 
-export const CardModal: React.FC<CardModalType> = ({title}) => {
+export const CardModal: React.FC<CardModalType> = ({_id, cardsPack_id, question, answer, title, isOpen, onClose}) => {
 
-    const {
-        _id,
-        cardsPack_id,
-        question,
-        answer,
-        openAddCardModal,
-        openEditCardModal
-    } = useAppSelector(state => state.modals)
     const dispatch = useAppDispatch()
-    const [questionValue, setQuestionValue] = useState(question)
-    const [answerValue, setAnswerValue] = useState(answer)
+    const [inputValues, setInputValues] = useState<string[]>([question, answer])
     const [questionType, setQuestionType] = useState('');
 
     const {
@@ -41,27 +38,22 @@ export const CardModal: React.FC<CardModalType> = ({title}) => {
         handleSubmit,
         reset,
         resetField,
-        formState,
-        formState: {errors, isSubmitSuccessful}
+        formState: {errors}
     } = useForm<CardModalFormData>({
         defaultValues: {
-            question: questionValue,
-            answer: answerValue
+            question: inputValues[0],
+            answer: inputValues[1]
         },
         mode: 'onSubmit'
     });
 
     const onSubmit: SubmitHandler<CardModalFormData> = data => {
-        debugger
         if (_id && (data.question !== question || data.answer !== answer)) {
-            debugger
             dispatch(updateCardTC({_id, ...data}))
         } else if (!_id) {
             dispatch(createCardTC({cardsPack_id, ...data}))
         }
         closeHandler()
-        console.log(data)
-        console.log(question, answer, _id)
     }
     const onEnterPress = (key: string) => {
         key === 'Enter' && handleSubmit(onSubmit)
@@ -69,16 +61,15 @@ export const CardModal: React.FC<CardModalType> = ({title}) => {
     const closeHandler = () => {
         resetField('question')
         resetField('answer')
-        dispatch(closeModalAC())
-        setQuestionValue('')
-        setAnswerValue('')
+        onClose()
+        setInputValues(['', ''])
     }
 
     const onQuestionChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setQuestionValue(e.currentTarget.value)
+        setInputValues([e.currentTarget.value, inputValues[1]])
     }
     const onAnswerChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setAnswerValue(e.currentTarget.value)
+        setInputValues([inputValues[0], e.currentTarget.value])
     }
 
     const handleChange = (event: SelectChangeEvent) => {
@@ -86,25 +77,12 @@ export const CardModal: React.FC<CardModalType> = ({title}) => {
     };
 
     useEffect(() => {
-        if (formState.isSubmitSuccessful) {
-            reset({
-                question: '',
-                answer: ''
-            })
-        }
-    }, [formState, isSubmitSuccessful, reset])
-
-    // это надо, потому что ты name берешь из редьюсера, а он не успевает подтянуться, поэтому в начале name = undefined
-    useEffect(() => {
-        setQuestionValue(question)
-        setAnswerValue(answer)
-
-        // это надо, потому что если без изменения нажать на кнопку 'Save', то будет ошибка, типа пустой инпут
+        setInputValues([question, answer])
         reset({question, answer})
     }, [question, answer, reset])
 
     return (
-        <BasicModal isOpen={openAddCardModal || openEditCardModal} onClose={closeHandler} title={title}>
+        <BasicModal isOpen={isOpen} onClose={closeHandler} title={title}>
             <form onSubmit={handleSubmit(onSubmit)}
                   onKeyDown={(e) => onEnterPress(e.key)}>
                 <FormGroup>
@@ -123,7 +101,7 @@ export const CardModal: React.FC<CardModalType> = ({title}) => {
 
                     <TextField label="Question"
                                variant="standard"
-                               value={questionValue}
+                               value={inputValues[0]}
                                sx={{marginTop: '25px'}}
                                {...register('question', {
                                    required: 'Question is required'
@@ -134,7 +112,7 @@ export const CardModal: React.FC<CardModalType> = ({title}) => {
 
                     <TextField label="Answer"
                                variant="standard"
-                               value={answerValue}
+                               value={inputValues[1]}
                                sx={{marginTop: '25px'}}
                                {...register('answer', {
                                    required: 'Answer is required'
