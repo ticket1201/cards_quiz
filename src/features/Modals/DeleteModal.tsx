0cad6b1865e31ecc2481dd52d6277093b0f40a62
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-05 21:48:49 +0300

    refactor: pass props to DeleteModal component

--- src/features/Modals/DeleteModal.tsx
+++ src/features/Modals/DeleteModal.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import {BasicModal} from '../../common/components/BasicModal/BasicModal';
 import Button from '@mui/material/Button';
-import {Stack} from '@mui/material';
-import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
+import Stack from '@mui/material/Stack';
+import {useAppDispatch} from '../../common/hooks/hooks';
 import {deletePackTC} from '../PacksList/pack_reducer';
-import {closeModalAC} from './modal_reducer';
 import {deleteCardTC} from '../PackPage/cards_reducer';
 
 type DeleteModalType = {
+    _id: string
+    name?: string
+    question?: string
     title: string
+    isOpen: boolean
+    onClose: () => void
 }
 
-export const DeleteModal: React.FC<DeleteModalType> = ({title}) => {
-    const {_id, name, question, openDelPackModal, openDelCardModal} = useAppSelector(state => state.modals)
+export const DeleteModal: React.FC<DeleteModalType> = ({_id, name, question, title, isOpen, onClose}) => {
     const dispatch = useAppDispatch()
 
     const deleteHandler = () => {
         question ? dispatch(deleteCardTC(_id)) : dispatch(deletePackTC(_id))
-        closeHandler()
-    }
-    const closeHandler = () => {
-        dispatch(closeModalAC())
+        onClose()
     }
 
     return (
-        <BasicModal isOpen={openDelPackModal || openDelCardModal} onClose={closeHandler} title={title}>
+        <BasicModal isOpen={isOpen} onClose={onClose} title={title}>
             <div>Do you really want to remove <b>{name || question}</b> ?</div>
             {title === 'Delete Pack'
                 ? <div>All cards will be deleted.</div>
                 : <div>This card will be deleted</div>}
             <Stack spacing={2} direction={'row'} justifyContent={'space-between'} sx={{marginTop: '26px'}}>
                 <Button variant={'outlined'}
-                        onClick={closeHandler}
+                        onClick={onClose}
                         sx={{width: '115px'}}>Cancel</Button>
                 <Button variant={'contained'}
                         color={'error'}
