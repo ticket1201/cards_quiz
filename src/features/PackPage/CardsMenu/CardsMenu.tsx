4fbc68428d0f12c986b1f0d0fc021a108ff01da5
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-05 21:52:24 +0300

    feat: add handlers and disable attribute to menu items

--- src/features/PackPage/CardsMenu/CardsMenu.tsx
+++ src/features/PackPage/CardsMenu/CardsMenu.tsx
@@ -7,7 +7,19 @@ import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 
-export const CardsMenu = () => {
+type CardsMenuType = {
+    isDisabled: boolean
+    editPackCallback: () => void
+    deletePackCallback: () => void
+    learnPackCallback: () => void
+}
+
+export const CardsMenu: React.FC<CardsMenuType> = ({
+                                                       isDisabled,
+                                                       editPackCallback,
+                                                       deletePackCallback,
+                                                       learnPackCallback
+                                                   }) => {
 
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
@@ -18,20 +30,45 @@ export const CardsMenu = () => {
         setAnchorEl(null);
     };
 
+    const editHandler = () => {
+        editPackCallback()
+        setAnchorEl(null)
+    }
+
+    const deleteHandler = () => {
+        deletePackCallback()
+        setAnchorEl(null)
+    }
+
+    const learnHandler = () => {
+        learnPackCallback()
+        setAnchorEl(null)
+    }
+
     return (
         <>
-            <IconButton
-                onClick={handleClick}
-            >
+            <IconButton onClick={handleClick}>
                 <MoreVert/>
             </IconButton>
             <Menu
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}>
-                <MenuItem onClick={handleClose}><BorderColorOutlinedIcon/> Edit</MenuItem>
-                <MenuItem onClick={handleClose}><DeleteOutlineOutlinedIcon/> Delete</MenuItem>
-                <MenuItem onClick={handleClose}><SchoolOutlinedIcon/> Learn</MenuItem>
+                <MenuItem onClick={editHandler}>
+                    <IconButton>
+                        <BorderColorOutlinedIcon/>
+                    </IconButton> Edit
+                </MenuItem>
+                <MenuItem onClick={deleteHandler}>
+                    <IconButton>
+                        <DeleteOutlineOutlinedIcon/>
+                    </IconButton> Delete
+                </MenuItem>
+                <MenuItem disabled={isDisabled} onClick={learnHandler}>
+                    <IconButton>
+                        <SchoolOutlinedIcon/>
+                    </IconButton> Learn
+                </MenuItem>
             </Menu>
         </>
     );
