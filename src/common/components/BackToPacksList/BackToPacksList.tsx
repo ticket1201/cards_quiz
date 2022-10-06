05e9e02d3c5cfb4ca72057a79c84cc27c24c23fc
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-06 23:47:49 +0300

    feat: create BackToPacksList component

---
+++ src/common/components/BackToPacksList/BackToPacksList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import {Path} from '../../enums/path';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import s from './BackToPacksList.module.css';
+import Typography from '@mui/material/Typography';
+
+
+export const BackToPacksList = () => {
+    return (
+        <div className={s.back}>
+            <NavLink to={`/${Path.PacksList}`} className={s.link}>
+                <Typography className={s.text}><KeyboardBackspaceIcon/> <span>Back to Packs List</span></Typography>
+            </NavLink>
+        </div>
+    );
+};
