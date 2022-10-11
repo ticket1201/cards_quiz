848b9766d0e9a1864937de0acdb503a2f92acffa
barash777 <barash.vertihvost@gmail.com>
2022-10-12 02:57:15 +0300

    experiment: trying to move search params outside

--- src/features/SearchBar/search-reducer.ts
+++ src/features/SearchBar/search-reducer.ts
@@ -1,42 +1,66 @@
+import {RootStateType} from "../../app/store";
+
 const initialState = {
     packName: undefined as string | undefined,
     user_id: undefined as string | undefined,
     min: undefined as number | undefined,
     max: undefined as number | undefined,
-    isReset: false
+    sortPacks: undefined as string | undefined,
+    cardsPack_id: undefined as string | undefined,
+    cardQuestion: undefined as string | undefined,
+    cardAnswer: undefined as string | undefined,
+    sortCards: undefined as string | undefined,
+
+    page: 1 as number | undefined,
+    pageCount: 10 as number | undefined,
 }
 
 export  type SearchInitialStateType = typeof initialState
 export const searchReducer = (state: SearchInitialStateType = initialState, action: SearchActionType): SearchInitialStateType => {
     switch (action.type) {
-        case 'search/SEARCH_PACKS':
-        case 'search/SORT_PACKS_BY_OWNER':
+        case 'search/SEARCH_PACKS_BY_NAME':
+        case 'search/SET_PACKS_BY_OWNER':
         case 'search/SET_RANGE':
             return {...state, ...action.payload}
         case 'search/CLEAR_FILTERS':
-            return {packName: '', user_id: '', min: undefined, max: undefined, isReset: true}
+            return {
+                packName: undefined,
+                user_id: undefined,
+                min: undefined,
+                max: undefined,
+                cardAnswer: undefined,
+                cardQuestion: undefined,
+                sortCards: undefined,
+                cardsPack_id: undefined,
+                sortPacks: undefined,
+                page: 1,
+                pageCount: 10
+            }
         default:
             return state
     }
 }
 
+// selector
+export const getSearchParams = (state: RootStateType): SearchInitialStateType => state.search;
+
 //types
 export type SearchActionType =
-    | ReturnType<typeof searchPacksAC>
-    | ReturnType<typeof sortPacksByOwnerAC>
+    | ReturnType<typeof searchPacksByNameAC>
+    | ReturnType<typeof setPacksByOwnerAC>
     | ReturnType<typeof setRangeAC>
     | ReturnType<typeof clearFiltersAC>
 
 // ACs
-export const searchPacksAC = (packName: string) => {
+export const searchPacksByNameAC = (packName: string) => {
     return {
-        type: 'search/SEARCH_PACKS',
+        type: 'search/SEARCH_PACKS_BY_NAME',
         payload: {packName}
     } as const
 }
-export const sortPacksByOwnerAC = (user_id: string | undefined = undefined) => {
+export const setPacksByOwnerAC = (user_id: string | undefined = undefined) => {
     return {
-        type: 'search/SORT_PACKS_BY_OWNER',
+        type: 'search/SET_PACKS_BY_OWNER',
         payload: {user_id}
     } as const
 }
@@ -46,6 +70,7 @@ export const setRangeAC = (min: number, max: number) => {
         payload: {min, max}
     } as const
 }
+
 export const clearFiltersAC = () => {
     return {
         type: 'search/CLEAR_FILTERS'
