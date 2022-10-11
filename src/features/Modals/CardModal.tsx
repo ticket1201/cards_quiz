1cd8be731781e95f7b31d7d9a038ef42dd91e18a
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-11 21:59:46 +0300

    feat: add image upload buttons to CardModal component

--- src/features/Modals/CardModal.tsx
+++ src/features/Modals/CardModal.tsx
@@ -11,7 +11,8 @@ import {useAppDispatch} from '../../common/hooks/hooks';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import {createCardTC, updateCardTC} from '../PackPage/cards_reducer';
-import {CommonModalStateType} from "./commonTypes";
+import {CommonModalStateType} from './commonTypes';
+import {UploadButton} from '../../common/components/UploadButton/UploadButton';
 
 type CardModalType = {
     data: CommonModalStateType
@@ -25,11 +26,13 @@ type CardModalFormData = {
 }
 
 export const CardModal: React.FC<CardModalType> = ({data, isOpen, onClose}) => {
-    const {_id, cardsPack_id, question, answer, title} = data
+    const {_id, cardsPack_id, question, answer, title, questionURL, answerURL} = data
 
     const dispatch = useAppDispatch()
     const [inputValues, setInputValues] = useState<string[]>([question, answer])
-    const [questionType, setQuestionType] = useState('');
+    const [questionType, setQuestionType] = useState('Text');
+    const [questionImg, setQuestionImg] = useState(questionURL)
+    const [answerImg, setAnswerImg] = useState(answerURL)
 
     const {
         register,
@@ -46,10 +49,10 @@ export const CardModal: React.FC<CardModalType> = ({data, isOpen, onClose}) => {
     });
 
     const onSubmit: SubmitHandler<CardModalFormData> = data => {
-        if (_id && (data.question !== question || data.answer !== answer)) {
-            dispatch(updateCardTC({_id, ...data}))
+        if (_id && (data.question !== question || data.answer !== answer || questionImg !== questionURL || answerImg !== answerURL)) {
+            dispatch(updateCardTC({_id, ...data, questionImg, answerImg}))
         } else if (!_id) {
-            dispatch(createCardTC({cardsPack_id, ...data}))
+            dispatch(createCardTC({cardsPack_id, ...data, questionImg, answerImg}))
         }
         closeHandler()
     }
@@ -58,7 +61,8 @@ export const CardModal: React.FC<CardModalType> = ({data, isOpen, onClose}) => {
         resetField('answer')
         onClose()
         setInputValues(['', ''])
-        setQuestionType('')
+        setQuestionImg('')
+        setAnswerImg('')
     }
 
     const onQuestionChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -74,15 +78,18 @@ export const CardModal: React.FC<CardModalType> = ({data, isOpen, onClose}) => {
 
     useEffect(() => {
         setInputValues([question, answer])
+        setQuestionImg(questionURL)
+        setAnswerImg(answerURL)
+        questionURL ? setQuestionType('Image') : setQuestionType('Text')
         reset({question, answer})
-    }, [question, answer, reset])
+    }, [question, answer, reset, questionURL, answerURL])
 
     return (
         <BasicModal isOpen={isOpen} onClose={closeHandler} title={title}>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <FormGroup>
 
-                    <FormControl fullWidth>
+                    {!questionURL && <FormControl fullWidth>
                         <InputLabel>Choose a question format</InputLabel>
                         <Select
                             value={questionType}
@@ -92,31 +99,40 @@ export const CardModal: React.FC<CardModalType> = ({data, isOpen, onClose}) => {
                             <MenuItem value={'Text'}>Text</MenuItem>
                             <MenuItem value={'Image'}>Image</MenuItem>
                         </Select>
-                    </FormControl>
+                    </FormControl>}
 
-                    <TextField label="Question"
-                               variant="standard"
-                               value={inputValues[0]}
-                               sx={{marginTop: '25px'}}
-                               {...register('question', {
-                                   required: 'Question is required'
-                               })}
-                               onChange={onQuestionChangeHandler}
-                    />
-                    {errors.question && <div style={{color: 'red'}}>{errors.question.message}</div>}
+                    {questionType === 'Text'
+                        ? <><TextField label="Question"
+                                       variant="standard"
+                                       value={inputValues[0]}
+                                       sx={{marginTop: '25px'}}
+                                       {...register('question', {
+                                           required: 'Question is required'
+                                       })}
+                                       onChange={onQuestionChangeHandler}
+                        />
+                            {errors.question && <div style={{color: 'red'}}>{errors.question.message}</div>}
 
-                    <TextField label="Answer"
-                               variant="standard"
-                               value={inputValues[1]}
-                               sx={{marginTop: '25px'}}
-                               {...register('answer', {
-                                   required: 'Answer is required'
-                               })}
-                               onChange={onAnswerChangeHandler}
-                    />
-                    {errors.answer && <div style={{color: 'red'}}>{errors.answer.message}</div>}
+                            <TextField label="Answer"
+                                       variant="standard"
+                                       value={inputValues[1]}
+                                       sx={{marginTop: '25px'}}
+                                       {...register('answer', {
+                                           required: 'Answer is required'
+                                       })}
+                                       onChange={onAnswerChangeHandler}
+                            />
+                            {errors.answer && <div style={{color: 'red'}}>{errors.answer.message}</div>}</>
+                        : <><UploadButton title={'Upload question image'}
+                                          label={'Question:'}
+                                          imgURL={questionImg}
+                                          saveImgUrl={setQuestionImg}/>
+                            <UploadButton title={'Upload answer image'}
+                                          label={'Answer:'}
+                                          imgURL={answerImg}
+                                          saveImgUrl={setAnswerImg}/></>}
 
-                    <Stack spacing={2} direction={'row'} justifyContent={'space-between'} sx={{marginTop: '50px'}}>
+                    <Stack spacing={2} direction={'row'} justifyContent={'space-between'} sx={{marginTop: '35px'}}>
                         <Button variant={'outlined'}
                                 onClick={() => closeHandler()}
                                 sx={{width: '115px'}}>Cancel</Button>
