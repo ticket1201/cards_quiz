cacbfdabf52b72ff8d01595833746d8c9224ce9a
Anton Metlitsky <ticket1201@gmail.com>
2022-10-12 21:05:44 +0400

    feat: implement profile dropdown menu logic

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Header.module.css'
 import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
-import {useAppSelector} from '../../hooks/hooks';
+import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
 import {NavLink, useNavigate} from 'react-router-dom';
 import ava from '../../../assets/images/avatar.jpg'
 import {Path} from '../../enums/path';
+import {logoutTC} from '../../../features/Login/auth_reducer';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const Header = () => {
     const navigate = useNavigate()
     const {name, avatar} = useAppSelector(state => state.auth)
+    const dispatch = useAppDispatch()
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const onMenuClickHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        setAnchorEl(e.currentTarget)
+    }
+    const onProfileClickHandler = () => {
+        menuCloseHandler()
+        navigate(`${Path.Profile}`)
+    }
+    const onLogoutClickHandler = () => {
+        menuCloseHandler()
+        dispatch(logoutTC())
+    }
+
+    const menuCloseHandler = () => {
+        setAnchorEl(null)
+    }
 
     return (
         <AppBar position={'absolute'} className={s.bar} style={{backgroundColor: '#FCFCFC'}}>
@@ -24,8 +48,15 @@ const Header = () => {
                     <NavLink className={s.logo} to={Path.Login}></NavLink>
                     {name
                         ? <div className={s.avaWrapper}>
-                            <NavLink to={Path.Profile}>{name}</NavLink>
-                            <NavLink className={s.ava} style={{backgroundImage:`url(${avatar || ava})`}} to={Path.Profile}></NavLink>
+                            <a onClick={onMenuClickHandler}>{name}</a>
+                            <NavLink className={s.ava} style={{backgroundImage: `url(${avatar || ava})`}}
+                                     to={Path.Profile}></NavLink>
+                            <Menu className={s.menu} open={open} onClose={menuCloseHandler} anchorEl={anchorEl}>
+                                <MenuItem onClick={onProfileClickHandler} className={s.menuItem}><PermIdentityIcon/> Profile</MenuItem>
+                                <MenuItem onClick={onLogoutClickHandler} className={s.menuItem}>
+                                  <LogoutOutlinedIcon/> <span>Log Out</span>
+                                </MenuItem>
+                            </Menu>
                         </div>
                         : <Button variant={'contained'} onClick={() => navigate(Path.Login)}>Sign in</Button>}
                 </Grid>
