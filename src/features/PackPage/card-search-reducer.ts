cf0584749e9e18db2500f0c28ecaa93d4d07d4cb
barash777 <barash.vertihvost@gmail.com>
2022-10-13 09:58:07 +0300

    feat: delete unnecessary properites from card-search-reducer.ts

--- src/features/PackPage/card-search-reducer.ts
+++ src/features/PackPage/card-search-reducer.ts
@@ -2,11 +2,8 @@ import {RootStateType} from "../../app/store";
 import {SortNameType} from "../SearchBar/search-constants";
 
 const initialState = {
-    packName: undefined as string | undefined,
-    user_id: undefined as string | undefined,
-    min: null as number | null,
-    max: null as number | null,
-    sortPacks: undefined as string | undefined,
+    // min: null as number | null,
+    // max: null as number | null,
     cardsPack_id: undefined as string | undefined,
     cardQuestion: undefined as string | undefined,
     cardAnswer: undefined as string | undefined,
@@ -16,18 +13,10 @@ const initialState = {
     pageCount: null as number | null,
 }
 
-export  type SearchInitialStateType = typeof initialState
-export const cardSearchReducer = (state: SearchInitialStateType = initialState, action: SearchActionType): SearchInitialStateType => {
+export  type SearchCardInitialStateType = typeof initialState
+export const cardSearchReducer = (state: SearchCardInitialStateType = initialState, action: SearchActionType): SearchCardInitialStateType => {
     switch (action.type) {
-        case 'search/SEARCH_PACKS_BY_NAME': {
-            let {packName} = action.payload
-            if (!packName) {
-                packName = undefined
-            }
-
-            return {...state, packName}
-        }
-        case 'search/SEARCH_CARDS_BY_QUESTION': {
+        case 'searchCard/SEARCH_CARDS_BY_QUESTION': {
             let {cardQuestion} = action.payload
             if (!cardQuestion) {
                 cardQuestion = undefined
@@ -35,21 +24,7 @@ export const cardSearchReducer = (state: SearchInitialStateType = initialState, 
 
             return {...state, cardQuestion}
         }
-        case 'search/SEARCH_PACKS_BY_OWNER': {
-            return {...state, ...action.payload}
-        }
-        case 'search/SEARCH_BY_RANGE': {
-            let {min, max, minCardsCount, maxCardsCount} = action.payload
-            if (min === minCardsCount) {
-                min = null
-            }
-            if (max === maxCardsCount) {
-                max = null
-            }
-
-            return {...state, min, max}
-        }
-        case "search/SET_SORT_PARAMS": {
+        case "searchCard/SET_SORT_PARAMS": {
             if (action.payload.sortName) {
                 return {
                     ...state,
@@ -58,21 +33,18 @@ export const cardSearchReducer = (state: SearchInitialStateType = initialState, 
             }
             return state
         }
-        case 'search/SET_PAGE':
-        case 'search/SET_PAGE_COUNT':
-        case 'search/SET_ALL':
+        case 'searchCard/SET_PAGE':
+        case 'searchCard/SET_PAGE_COUNT':
+        case 'searchCard/SET_ALL':
             return {...state, ...action.payload}
-        case 'search/CLEAR_FILTERS':
+        case 'searchCard/CLEAR_FILTERS':
             return {
-                packName: undefined,
-                user_id: undefined,
-                min: null,
-                max: null,
+                // min: null,
+                // max: null,
                 cardAnswer: undefined,
                 cardQuestion: undefined,
                 sortCards: undefined,
                 cardsPack_id: undefined,
-                sortPacks: undefined,
                 page: null,
                 pageCount: null,
             }
@@ -82,14 +54,12 @@ export const cardSearchReducer = (state: SearchInitialStateType = initialState, 
 }
 
 // selector
-export const selectCardSearchParams = (state: RootStateType): SearchInitialStateType => state.cardSearch;
+export const selectCardSearchParams = (state: RootStateType): SearchCardInitialStateType => state.cardSearch;
 
 //types
 export type SearchActionType =
-    | ReturnType<typeof searchPacksByNameAC>
     | ReturnType<typeof searchCardsByQuestionAC>
-    | ReturnType<typeof searchPacksByOwnerAC>
-    | ReturnType<typeof searchByRangeAC>
+    // | ReturnType<typeof searchByRangeAC>
     | ReturnType<typeof setPageAC>
     | ReturnType<typeof setPageCountAC>
     | ReturnType<typeof setSortParamsAC>
@@ -97,57 +67,44 @@ export type SearchActionType =
     | ReturnType<typeof setAllAC>
 
 // ACs
-export const searchPacksByNameAC = (packName: string | undefined) => {
-    return {
-        type: 'search/SEARCH_PACKS_BY_NAME',
-        payload: {packName}
-    } as const
-}
 export const searchCardsByQuestionAC = (cardQuestion: string | undefined) => {
     return {
-        type: 'search/SEARCH_CARDS_BY_QUESTION',
+        type: 'searchCard/SEARCH_CARDS_BY_QUESTION',
         payload: {cardQuestion}
     } as const
 }
-export const searchPacksByOwnerAC = (user_id: string | undefined = undefined) => {
+/*export const searchByRangeAC = (min: number | null, max: number | null, minCardsCount: number, maxCardsCount: number) => {
     return {
-        type: 'search/SEARCH_PACKS_BY_OWNER',
-        payload: {user_id}
-    } as const
-}
-export const searchByRangeAC = (min: number | null, max: number | null, minCardsCount: number, maxCardsCount: number) => {
-    return {
-        type: 'search/SEARCH_BY_RANGE',
+        type: 'searchCard/SEARCH_BY_RANGE',
         payload: {min, max, minCardsCount, maxCardsCount}
     } as const
-}
+}*/
 export const setPageAC = (page: number | null) => {
     return {
-        type: 'search/SET_PAGE',
+        type: 'searchCard/SET_PAGE',
         payload: {page}
     } as const
 }
 export const setPageCountAC = (pageCount: number | null) => {
     return {
-        type: 'search/SET_PAGE_COUNT',
+        type: 'searchCard/SET_PAGE_COUNT',
         payload: {pageCount}
     } as const
 }
 export const setSortParamsAC = (sortName: SortNameType, sortValue: string | undefined) => {
     return {
-        type: 'search/SET_SORT_PARAMS',
+        type: 'searchCard/SET_SORT_PARAMS',
         payload: {sortName, sortValue}
     } as const
 }
 export const clearSearchFiltersAC = () => {
     return {
-        type: 'search/CLEAR_FILTERS'
+        type: 'searchCard/CLEAR_FILTERS'
     } as const
 }
-
 export const setAllAC = (all: any) => {
     return {
-        type: 'search/SET_ALL',
+        type: 'searchCard/SET_ALL',
         payload: all
     } as const
 }
